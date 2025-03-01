802decc9074bd1d11eb280338621f101a5994987
Yogesh Linganna <8181395+yogeshl@users.noreply.github.com>
2025-03-01 08:53:36 -0500

    Update docker-install-ubuntu.sh

--- docker-install-ubuntu.sh
+++ docker-install-ubuntu.sh
@@ -21,17 +21,20 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 # 5. Verify that you now have the Docker GPG key
 sudo apt-key fingerprint 0EBFCD88
 
-# 6. Add the Docker repository to apt
-sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
+# 6. Add the Docker repository to apt sources
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # 7. Re-Update the apt-package index
 sudo apt-get update
 
+# Make sure you are about to install from the Docker repo instead of the default Ubuntu repo
+apt-cache policy docker-ce
+
 # 8. Install a specific version of docker
 sudo apt-get install docker-ce
+
+# Check status
+sudo systemctl status docker
 
 # 9. Add groups and users
 sudo groupadd docker
