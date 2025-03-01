a3a27289f3dd3c29bb55e5ac80951917edb034e4
Yogesh Linganna <8181395+yogeshl@users.noreply.github.com>
2025-03-01 08:43:34 -0500

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -1,3 +1,4 @@
+# Ref: https://www.digitalocean.com/community/tutorials/how-to-enable-remote-desktop-protocol-using-xrdp-on-ubuntu-22-04
 # How To Enable Remote Desktop Protocol Using xrdp on Ubuntu 
 sudo apt update
 
